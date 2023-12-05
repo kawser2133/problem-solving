ca7f0705578ce953b7e73548e48f4ff9fd433007
kawser2133 <kawser2133@gmail.com>
2023-12-05 20:57:14 +0600

    Overloading Example

---
+++ ProblemSolvingPractice/ProblemSolvingPractice/OverloadingExample.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace ProblemSolvingPractice
+{
+    public class OverloadingExample
+    {
+        // Method with two integer parameters
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Method with three integer parameters
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        // Method with two double parameters
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Method with a string parameter
+        public string Add(string a, string b)
+        {
+            return a + b;
+        }
+    }
+
+}
