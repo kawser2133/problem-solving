1b0b5ed1e8e15d0999497d81061763f5061fe416
kawser2133 <kawser2133@gmail.com>
2023-12-06 22:23:51 +0600

    Overriding  example

---
+++ ProblemSolvingPractice/ProblemSolvingPractice/OverridingExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Base class
+class Animal
+{
+    // Virtual method
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a generic sound.");
+    }
+}
+
+// Derived class
+class Dog : Animal
+{
+    // Override the MakeSound method from the base class
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks: Woof! Woof!");
+    }
+}
+
+// Another derived class
+class Cat : Animal
+{
+    // Override the MakeSound method from the base class
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows: Meow! Meow!");
+    }
+}
